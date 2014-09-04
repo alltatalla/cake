95cead4d221a06f8917dc5aa1b5bc6a33bf0d2cb
Anders Sundman <anders@4zm.org>
2014-09-04 14:39:14 +0200

    Added canvas

--- app/play/play-ctrl.js
+++ app/play/play-ctrl.js
@@ -2,9 +2,26 @@
 
 angular.module('cakeApp')
   .controller('PlayCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
+    $scope.foes = [];
+
+    var drawSprite = function(s) {
+        context.beginPath();
+        context.arc(100, 200, 10, 0, 2*Math.PI, false);
+        context.fillStyle = "#ccddff";
+        context.fill();
+        context.lineWidth = 1;
+        context.strokeStyle = "#666666";
+        context.stroke();
+    };
+
+    // setup
+    canvas.width = 640;
+    canvas.height = 480;
+    context.globalAlpha = 1.0;
+    context.beginPath();
+    drawSprite(true);
+});
